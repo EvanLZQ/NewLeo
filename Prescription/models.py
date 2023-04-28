2956d326de469c73ea95c26f238bee997dac1519
Evanabyss <evanabyss@gmail.com>
2023-04-27 22:10:46 -0400

    Change to Prescription model

--- Prescription/models.py
+++ Prescription/models.py
@@ -6,17 +6,17 @@ __all__ = ['PrescriptionInfo', 'PrescriptionPrism']
 class PrescriptionInfo(models.Model):
     pd_l = models.DecimalField(max_digits=5, decimal_places=2)
     pd_r = models.DecimalField(max_digits=5, decimal_places=2)
-    sphere_l = models.DecimalField(max_digits=5, decimal_places=2)
-    sphere_r = models.DecimalField(max_digits=5, decimal_places=2)
-    cylinder_l = models.DecimalField(max_digits=5, decimal_places=2)
-    cylinder_r = models.DecimalField(max_digits=5, decimal_places=2)
+    sphere_l = models.DecimalField(max_digits=5, decimal_places=2, blank=True)
+    sphere_r = models.DecimalField(max_digits=5, decimal_places=2, blank=True)
+    cylinder_l = models.DecimalField(
+        max_digits=5, decimal_places=2, blank=True)
+    cylinder_r = models.DecimalField(
+        max_digits=5, decimal_places=2, blank=True)
     axis_l = models.IntegerField(blank=True)
     axis_r = models.IntegerField(blank=True)
-    prism_l = models.DecimalField(max_digits=5, decimal_places=2)
-    prism_r = models.DecimalField(max_digits=5, decimal_places=2)
-    base_l = models.DecimalField(max_digits=5, decimal_places=2)
-    base_r = models.DecimalField(max_digits=5, decimal_places=2)
-    nv_add = models.DecimalField(max_digits=5, decimal_places=2)
+    base_l = models.DecimalField(max_digits=5, decimal_places=2, blank=True)
+    base_r = models.DecimalField(max_digits=5, decimal_places=2, blank=True)
+    nv_add = models.DecimalField(max_digits=5, decimal_places=2, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
