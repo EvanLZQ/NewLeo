cdc13e4c7a5e182ca716811dabe4329acb06bc0e
Evanabyss <evanabyss@gmail.com>
2023-06-11 11:21:56 -0400

    fetch single product

--- Product/urls.py
+++ Product/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('products/', views.getProducts, name='products'),
-    # path('products/dimensions/', views.getProductDimension, name="productdimension")
+    path('products/<str:sku>/', views.getProduct, name='product_details'),
 ]
--- Product/views.py
+++ Product/views.py
@@ -15,8 +15,17 @@ def all_products(request):
 
 @api_view(['GET'])
 def getProducts(request):
-    products = ProductInfo.objects.select_related('dimensionID').all()
+    products = ProductInfo.objects.select_related(
+        'dimensionID').filter(online=True)
     serializer = ProductSerializer(products, many=True)
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+def getProduct(request, sku):
+    products = ProductInfo.objects.select_related(
+        'dimensionID').get(sku=sku)
+    serializer = ProductSerializer(products, many=False)
     return Response(serializer.data)
 
 
