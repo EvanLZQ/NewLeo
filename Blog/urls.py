1326a6a39121b25d11f1e9c902557f6bc715cf3a
Evanabyss <evanabyss@gmail.com>
2023-10-24 21:20:54 -0400

    Update Blog

--- Blog/urls.py
+++ Blog/urls.py
@@ -5,4 +5,7 @@ urlpatterns = [
     path('brief', views.getBlogBrief, name='blog_brief'),
     path('all_blog', views.getAllBlogs, name='all_blogs'),
     path('blog/<int:blog_id>', views.getBlogDetails, name='blog_details'),
+    path('home_page_blog', views.getFirstBlogBrief, name='home_page_blog'),
+    path('target_blog_brief/<int:blog_id>',
+         views.getTargetBlogBrief, name='target_blog_brief'),
 ]
