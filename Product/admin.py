d05441d5ce498a7fe75393eeef4631d94786b81f
Evanabyss <evanabyss@gmail.com>
2023-10-26 13:39:48 -0400

    update product

--- Product/admin.py
+++ Product/admin.py
@@ -8,28 +8,28 @@ class ProductTagInline(admin.TabularInline):
     model = ProductTag.product.through
 
 
-class ProductInfoForm(forms.ModelForm):
-    tags = forms.ModelMultipleChoiceField(
-        queryset=ProductTag.objects.all(),
-        required=False,
-        widget=admin.widgets.FilteredSelectMultiple('tags', False)
-    )
+# class ProductInfoForm(forms.ModelForm):
+#     tags = forms.ModelMultipleChoiceField(
+#         queryset=ProductTag.objects.all(),
+#         required=False,
+#         widget=admin.widgets.FilteredSelectMultiple('tags', False)
+#     )
 
-    class Meta:
-        model = ProductInfo
-        fields = '__all__'
+#     class Meta:
+#         model = ProductInfo
+#         fields = '__all__'
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        if self.instance:
-            self.fields['tags'].initial = self.instance.producttag_set.all()
+#     def __init__(self, *args, **kwargs):
+#         super().__init__(*args, **kwargs)
+#         if self.instance:
+#             self.fields['tags'].initial = self.instance.producttag_set.all()
 
-    def save(self, *args, **kwargs):
-        instance = super().save(*args, **kwargs)
-        self.save_m2m()
-        if instance.pk:
-            instance.producttag_set.set(self.cleaned_data['tags'])
-        return instance
+#     def save(self, *args, **kwargs):
+#         instance = super().save(*args, **kwargs)
+#         self.save_m2m()
+#         if instance.pk:
+#             instance.producttag_set.set(self.cleaned_data['tags'])
+#         return instance
 
 
 class ProductImageInline(admin.StackedInline):
@@ -64,13 +64,21 @@ class ProductInstanceAdmin(admin.ModelAdmin):
 
 
 class ProductInfoAdmin(admin.ModelAdmin):
-    inlines = [ProductInstanceInline]
-    form = ProductInfoForm
+    inlines = [ProductInstanceInline, ProductTagInline]
 
-    def save_model(self, request, obj, form, change):
-        super().save_model(request, obj, form, change)
-        if not change:
-            form.save_m2m()
+    def get_inline_instances(self, request, obj=None):
+        inline_instances = super().get_inline_instances(request, obj)
+        for inline_instance in inline_instances:
+            if isinstance(inline_instance, ProductTagInline):
+                inline_instance.verbose_name = "Product Tag"
+                inline_instance.verbose_name_plural = "Product Tags"
+        return inline_instances
+    # form = ProductInfoForm
+
+    # def save_model(self, request, obj, form, change):
+    #     super().save_model(request, obj, form, change)
+    #     if not change:
+    #         form.save_m2m()
 
 
 admin.site.register(ProductInfo, ProductInfoAdmin)
