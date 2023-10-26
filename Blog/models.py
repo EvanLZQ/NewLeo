dfbe46f083abfb7a0d863f7153397ef19da7d83e
Evanabyss <evanabyss@gmail.com>
2023-10-25 22:02:41 -0400

    update product image

--- Blog/models.py
+++ Blog/models.py
@@ -8,7 +8,8 @@ class BlogInfo(models.Model):
     content = QuillField()
     sub_title = models.CharField(max_length=200, blank=True, null=True)
     brief = models.TextField(blank=True, null=True)
-    home_page_img = models.ImageField(blank=True, null=True)
+    home_page_img = models.ImageField(
+        upload_to='blog_images/', blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
