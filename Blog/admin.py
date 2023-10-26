bef9f36a219df2736f7782c2ce378531a6f1d9cd
Evanabyss <evanabyss@gmail.com>
2023-10-25 20:24:40 -0400

    update blog

--- Blog/admin.py
+++ Blog/admin.py
@@ -7,11 +7,6 @@ from django.utils.safestring import mark_safe
 class BlogInfoAdmin(admin.ModelAdmin):
     list_display = ('title', 'created_at')
     ordering = ('-created_at',)
-    readonly_fields = ('home_page_img_preview',)
-
-    def home_page_img_preview(self, obj):
-        return mark_safe(f'<img src="http://admin.eyelovewear.com/{obj.home_page_img}" style="max-width: 300px; margin: 5px;">')
-    home_page_img_preview.short_description = 'Home Page Image Preview'
 
 
 admin.site.register(BlogInfo, BlogInfoAdmin)
