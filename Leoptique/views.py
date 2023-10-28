b8c90e4bb328b3926b154e249e6bd5983067bf2c
Evanabyss <evanabyss@gmail.com>
2023-10-28 00:18:45 -0400

    update tinymce

--- Leoptique/views.py
+++ Leoptique/views.py
@@ -1,6 +1,8 @@
 from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 
 
+@csrf_exempt
 def upload_image(request):
     if request.method == 'POST':
         image_file = request.FILES.get('file')
