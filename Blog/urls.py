f605017d913220893196f7bdf6d3260f76e562a8
Evanabyss <evanabyss@gmail.com>
2023-11-20 10:04:08 -0500

    update blog

--- Blog/urls.py
+++ Blog/urls.py
@@ -4,8 +4,8 @@ from . import views
 urlpatterns = [
     path('brief', views.getBlogBrief, name='blog_brief'),
     path('all_blog', views.getAllBlogs, name='all_blogs'),
-    path('blog/<int:blog_id>', views.getBlogDetails, name='blog_details'),
+    path('blog/<slug:blog_slug>', views.getBlogDetails, name='blog_details'),
     path('home_page_blog', views.getFirstBlogBrief, name='home_page_blog'),
-    path('target_blog_brief/<int:blog_id>',
+    path('target_blog_brief/<slug:blog_slug>',
          views.getTargetBlogBrief, name='target_blog_brief'),
 ]
