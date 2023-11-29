296327ef394071fae0c43a8a183082fcb913698d
Evanabyss <evanabyss@gmail.com>
2023-11-28 22:28:17 -0500

    update prescription

--- Prescription/models.py
+++ Prescription/models.py
@@ -17,8 +17,10 @@ class PrescriptionInfo(models.Model):
         max_digits=5, decimal_places=2, blank=True, default=0)
     cylinder_r = models.DecimalField(
         max_digits=5, decimal_places=2, blank=True, default=0)
-    axis_l = models.IntegerField(blank=True, default=0)
-    axis_r = models.IntegerField(blank=True, default=0)
+    axis_l = models.DecimalField(
+        max_digits=5, decimal_places=2, blank=True, default=0)
+    axis_r = models.DecimalField(
+        max_digits=5, decimal_places=2, blank=True, default=0)
     add_l = models.DecimalField(
         blank=True, default=0, max_digits=5, decimal_places=2)
     add_r = models.DecimalField(
