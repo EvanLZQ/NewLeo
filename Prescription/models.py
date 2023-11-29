5fb1eed58066516283f688a7ff86d38ee002d2d5
Evanabyss <evanabyss@gmail.com>
2023-11-28 22:19:56 -0500

    update prescription model

--- Prescription/models.py
+++ Prescription/models.py
@@ -19,12 +19,14 @@ class PrescriptionInfo(models.Model):
         max_digits=5, decimal_places=2, blank=True, default=0)
     axis_l = models.IntegerField(blank=True, default=0)
     axis_r = models.IntegerField(blank=True, default=0)
+    add_l = models.DecimalField(
+        blank=True, default=0, max_digits=5, decimal_places=2)
+    add_r = models.DecimalField(
+        blank=True, default=0, max_digits=5, decimal_places=2)
     base_l = models.DecimalField(
         max_digits=5, decimal_places=2, blank=True, default=0)
     base_r = models.DecimalField(
         max_digits=5, decimal_places=2, blank=True, default=0)
-    nv_add = models.DecimalField(
-        max_digits=5, decimal_places=2, blank=True,    default=0)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
