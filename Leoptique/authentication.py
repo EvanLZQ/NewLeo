6e65508fc004bf62ac6a79f6211aa37076d38716
Evanabyss <evanabyss@gmail.com>
2026-02-12 20:51:38 -0500

    fix login bugs

--- Leoptique/authentication.py
+++ Leoptique/authentication.py
@@ -5,31 +5,24 @@ from oauth2_provider.models import AccessToken
 
 class AccessTokenAuthentication(BaseAuthentication):
     def authenticate(self, request):
-        access_token = request.COOKIES.get('access_token')
+        token_value = request.COOKIES.get("access_token")
 
-        if not access_token:
+        # Optional: allow Bearer token too
+        if not token_value:
+            auth = request.headers.get("Authorization", "")
+            if auth.startswith("Bearer "):
+                token_value = auth.split(" ", 1)[1].strip()
+
+        if not token_value:
             return None
 
         try:
-            token = AccessToken.objects.get(token=access_token)
+            token = AccessToken.objects.select_related(
+                "user").get(token=token_value)
 
             if token.is_expired():
-                raise AuthenticationFailed('Access token has expired')
+                raise AuthenticationFailed("Access token has expired")
 
             return (token.user, token)
         except AccessToken.DoesNotExist:
-            raise AuthenticationFailed('No such token')
-
-
-# from rest_framework_simplejwt.authentication import JWTAuthentication
-
-
-# class CookieJWTAuthentication(JWTAuthentication):
-#     def authenticate(self, request):
-#         raw_token = request.COOKIES.get('access_token')
-#         if raw_token is None:
-#             return None
-
-#         validated_token = self.get_validated_token(raw_token)
-#         print(f"validate token: {validated_token}")
-#         return self.get_user(validated_token), validated_token
+            raise AuthenticationFailed("Invalid access token")
